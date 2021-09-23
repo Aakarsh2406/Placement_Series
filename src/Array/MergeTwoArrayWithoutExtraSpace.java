96579622d94b852472cf2001e1435f1471f5b4a7
aakarsh2406 <akarsh.gangwar_cs18@gla.ac.in>
2021-09-23 08:51:24 +0530

    Code error fixed

--- src/Array/MergeTwoArrayWithoutExtraSpace.java
+++ src/Array/MergeTwoArrayWithoutExtraSpace.java
@@ -4,33 +4,38 @@ import java.util.Arrays;
 
 public class MergeTwoArrayWithoutExtraSpace {
 
-    static void merger(int arr1[], int arr2[]) {
+    static String merger(int[] arr1, int[] arr2) {
+
+        if(arr1.length==0)
+            return Arrays.toString(arr2);
+        if(arr2.length==0)
+            return Arrays.toString(arr1);
 
         int temp=0;
         int i=arr1.length-1;
         System.out.println(i);
         int j=0;
-        while (i>=0 && j< arr2.length)
+        while (i>=0 && j<= arr2.length-1)
         {
             System.out.println(i+" "+j);
             if(arr1[i]>arr2[j])
             {
-                temp=arr1[arr1.length-1];
-                arr1[arr1.length-1]=arr2[j];
+                temp=arr1[i];
+                arr1[i]=arr2[j];
                 arr2[j]=temp;
 
             }
             i--;
             j++;
         }
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        System.out.println(Arrays.toString(arr1)+" "+Arrays.toString(arr2));
+       Arrays.sort(arr1);
+       Arrays.sort(arr2);
+        return Arrays.toString(arr1)+" "+Arrays.toString(arr2);
     }
 
     public static void main(String[] args) {
-        int arr1[]={1,3,5,8,10,89};
-        int arr2[]={2,4,6};
-        merger(arr1,arr2);
+        int arr1[]={3,5,4};
+        int arr2[]={1,2,3};
+        System.out.println( merger(arr1,arr2));
     }
 }
